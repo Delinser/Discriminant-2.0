97a96f3dbbc9eea4deb90bfbd70224ae305a83b0
Delinser <a.n.yalanzhi@gmail.com>
2022-02-03 02:16:05 +1000

    1

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@
             let Dsq = Math.sqrt(D)
            
                 if(A == ""){
-                    alert("ИДИ на хуй")
+                    alert("NoN")
 
                 }
                 else{
