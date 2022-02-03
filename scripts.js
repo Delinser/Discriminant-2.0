825f0d0e4caab3d5457402ff38af068f78a05321
Delinser <a.n.yalanzhi@gmail.com>
2022-02-03 17:49:28 +1000

    1

--- scripts.js
+++ scripts.js
@@ -18,24 +18,29 @@
                 let X2 = (-B - Math.sqrt(D))/ (2*A)
                 let B_sqrt =  B**2
                 let Dsq = Math.sqrt(D)
-           
-                if(A == ""){                                                                    //output 
-                    alert("NoN")
+                if((A == "" && B == "")&& C==""){
+                    alert("enter numbers")
+                }
+                else{
+                    if(A == ""){                                                                    //output 
+                        alert("NoN")
 
-                }
-                else{                                                                           
-                    if(D>0){
-                    alert("D=" + B_sqrt + "-4*" + A +"*" + C + "=" + D + '\n' + "X1=" + "(" + -B + "+" + Dsq + ")" + "/" + "(" + A + "*2" + ")" + "=" + X1 + '\n' + "X2=" + "(" + -B + "-" + Dsq + ")/("+  A + "*2"+ ")"+ "=" + X2  )
                     }
-                    else{
-                        if(X1 == X2){
-                            alert("D=" +  B_sqrt + "-4*" + A + "*" + C + "=" + D + '\n' + "X12=" + "(" + -B + "+" + Dsq + ")" + "/" + "(" + A + "*2)"+ "=" + X1  )
+                    else{                                                                           
+                        if(D>0){
+                        alert("D=" + B_sqrt + "-4*" + A +"*" + C + "=" + D + '\n' + "X1=" + "(" + -B + "+" + Dsq + ")" + "/" + "(" + A + "*2" + ")" + "=" + X1 + '\n' + "X2=" + "(" + -B + "-" + Dsq + ")/("+  A + "*2"+ ")"+ "=" + X2  )
                         }
-                        else {
-                            alert("D=" + B_sqrt + "-4*" + A +"*" + C + "=" + D  +  '\n' +  "Nan")
+                        else{
+                            if(X1 == X2){
+                                alert("D=" +  B_sqrt + "-4*" + A + "*" + C + "=" + D + '\n' + "X12=" + "(" + -B + "+" + Dsq + ")" + "/" + "(" + A + "*2)"+ "=" + X1  )
+                            }
+                            else {
+                                alert("D=" + B_sqrt + "-4*" + A +"*" + C + "=" + D  +  '\n' +  "Nan")
+                            }
                         }
                     }
                 }
+               
             };
             
                 clearButton.onclick = function clearf(){                                        //Clear values in inputs(dis)
@@ -55,21 +60,23 @@
             let c = Math.sqrt(((A1**2) + (B1**2)))
             let a = Math.sqrt(((C1**2) - (B1**2)))
             let b = Math.sqrt(((C1**2) - (A1**2)))
-           
-            if(C1 == ""){ 
-                
-                alert("C="+c)
+            if((A1 =="" && B1 =="") && C1 == ""){
+                alert("enter numbers")
             }
-            if(A1 == ""){
-                
-                alert("A="+a) 
+            else{
+                if(C1 == ""){ 
+                    
+                    alert("C="+c)
+                }
+                if(A1 == ""){
+                    
+                    alert("A="+a) 
+                }
+                if(B1 == ""){
+                    
+                    alert("B="+b)
+                }
             }
-            if(B1 == ""){
-                
-                alert("B="+b)
-            }
-
-
                };                              
             clearButton1.onclick = function clearf1(){                                         //Clear values in inputs(sqrt)
                 document.getElementById("inp_4").value = ""
@@ -78,15 +85,14 @@
             };// End of code
 
             countButton2.onclick = function sint(){
-
             let A2 = document.getElementById('inp_7').value 
             let B2 = document.getElementById('inp_8').value 
             let C2 = document.getElementById('inp_9').value 
             let D2 = document.getElementById('inp_10').value 
             let E2 = document.getElementById('inp_11').value 
             let F2 = document.getElementById('inp_12').value 
-            if((A2 == "" && B2 == "") && C2 == ""){
-                alert("")
+            if(((A2 == "" && B2 == "") && (C2 == "")) || ((D2 == "" && E2 == "") && (F2 == "")) ){
+                alert("Enter at least one number ")
             }
 
 
@@ -94,7 +100,7 @@
            
 
             };                              
-            clearButton2.onclick = function clearf2(){                                         //Clear values in inputs(sqrt)
+            clearButton2.onclick = function clearf2(){                                         //Clear values in inputs(sint)
                 document.getElementById("inp_7").value = ""
                 document.getElementById("inp_8").value = ""
                 document.getElementById("inp_9").value = ""
