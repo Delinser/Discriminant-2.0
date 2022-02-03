e4f1ad68a88e2c2a3953ede6126d6e167c981851
Delinser <a.n.yalanzhi@gmail.com>
2022-02-03 18:28:03 +1000

    1

--- scripts.js
+++ scripts.js
@@ -91,9 +91,43 @@
             let D2 = document.getElementById('inp_10').value 
             let E2 = document.getElementById('inp_11').value 
             let F2 = document.getElementById('inp_12').value 
+            let i = 0
             if(((A2 == "" && B2 == "") && (C2 == "")) || ((D2 == "" && E2 == "") && (F2 == "")) ){
                 alert("Enter at least one number ")
             }
+            if(((A2 == "" || B2== "")|| C2 == "") && (D2!= "" || E2 != "") || F2 != ""){
+                alert("vse zbs")
+
+            }
+            else{
+                alert("Enter the required number")
+            }   
+            
+                if(A2 != ""){
+                    i = i + 1
+                }
+                if(B2 != ""){
+                    i = i + 1
+                }
+                if(C2 != ""){
+                    i = i + 1
+                }
+                if(D2 != ""){
+                    i = i + 1
+                }
+                if(E2 != ""){
+                    i = i + 1
+                }
+                if(F2 != ""){
+                    i = i + 1
+                }
+                if(i<3){
+                    alert("Enter the required number ")                                                            
+                }
+                else{
+                    alert("i="+ i)                                                        // вот сюда
+                }
+            
 
 
         
@@ -108,8 +142,9 @@
                 document.getElementById("inp_11").value = ""
                 document.getElementById("inp_12").value = ""
             };// End of code
+                //for(let i = 0;1<0;)
                 
-                
+              
 
     
       
