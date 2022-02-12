38a6c71194496d47da3fc6b124459ae5243b0786
Delinser <a.n.yalanzhi@gmail.com>
2022-02-12 19:47:28 +1000

    1

---
+++ Page_Discriminant/script_Discriminant.js
@@ -0,0 +1,47 @@
+window.onload = function clear__all__after__update() {
+    document.getElementById("inp_1").value = ""
+    document.getElementById("inp_2").value = ""
+    document.getElementById("inp_3").value = ""  
+ }; 
+
+countButton.onclick = function dis()   {                                                 // Pull values from inputs(dis)
+
+let A = document.getElementById('inp_1').value 
+let B = document.getElementById('inp_2').value 
+let C = document.getElementById('inp_3').value 
+
+let D = B**2 - 4*A*C
+let X1 = (-B + Math.sqrt(D))/ (2*A)
+let X2 = (-B - Math.sqrt(D))/ (2*A)
+let B_sqrt =  B**2
+let Dsq = Math.sqrt(D)
+if((A == "" && B == "")&& C==""){
+    alert("enter numbers")
+}
+else{
+    if(A == ""){                                                                    //output 
+        alert("NoN")
+
+    }
+    else{                                                                           
+        if(D>0){
+        alert("D=" + B_sqrt + "-4*" + A +"*" + C + "=" + D + '\n' + "X1=" + "(" + -B + "+" + Dsq + ")" + "/" + "(" + A + "*2" + ")" + "=" + X1 + '\n' + "X2=" + "(" + -B + "-" + Dsq + ")/("+  A + "*2"+ ")"+ "=" + X2  )
+        }
+        else{
+            if(X1 == X2){
+                alert("D=" +  B_sqrt + "-4*" + A + "*" + C + "=" + D + '\n' + "X12=" + "(" + -B + "+" + Dsq + ")" + "/" + "(" + A + "*2)"+ "=" + X1  )
+            }
+            else {
+                alert("D=" + B_sqrt + "-4*" + A +"*" + C + "=" + D  +  '\n' +  "Nan")
+            }
+        }
+    }
+}
+
+};
+
+clearButton.onclick = function clearf(){                                        //Clear values in inputs(dis)
+    document.getElementById("inp_1").value = ""
+    document.getElementById("inp_2").value = ""
+    document.getElementById("inp_3").value = ""
+};
